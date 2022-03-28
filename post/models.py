d46ecccc288b747dfda8170f4f07505369b15beb
YangTaeyoung <0130yang@gmail.com>
2022-03-29 03:35:53 +0900

    [Modify] PostTag n:n 테이블 제거 -> ManyToManyField로 수정

--- post/models.py
+++ post/models.py
@@ -13,15 +13,9 @@ class Tag(models.Model):
     description = models.TextField()
 
 
-# 게시물-태그 매핑 모델
-class PostTags(models.Model):
-    # 게시글이나 태그를 삭제할 때 관련된 매핑 레코드 삭제
-    post = models.ForeignKey('Post', related_name='post_tag_post', on_delete=models.CASCADE)
-    tag = models.ForeignKey('Tag', related_name='post_tag_tag', on_delete=models.CASCADE)
-
-
 # 게시물 모델
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     type = models.ForeignKey('Type', related_name="post_type", on_delete=models.CASCADE)  # 게시물의 타입은 여러개를 가질 수 있음.
+    tags = models.ManyToManyField("Tag", related_name="posts")
